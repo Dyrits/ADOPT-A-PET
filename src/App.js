15548d4cc51a4193b13cd51f69f3174ca44df30f
Dylan J. Gerrits <dyrits@outlook.com>
2023-07-30 13:40:37 +0200

    🎉 Initialization

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Root /> }>
+      <Route index element={<HomePage /> } />
+  </Route>
+));
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App
