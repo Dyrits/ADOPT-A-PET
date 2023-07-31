78c720f882e474a82b95808bcc75035f49475ee1
Dylan J. Gerrits <dyrits@outlook.com>
2023-07-31 13:00:13 +0200

    ✨ Implementation of the navigation

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import PetDetailsNotFound from "./pages/petDetailsNotFound";
 import Root from "./components/root";
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Root /> }>
-      <Route index element={<HomePage /> } />
+  <Route path="/" element={<Root />}>
+    <Route index element={<HomePage />} />
+    <Route path=":type" element={<HomePage />} />
+    <Route path=":type/:id" element={<PetDetailsPage />} />
   </Route>
 ));
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
